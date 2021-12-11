561ecbb02308d7a287d5d8fa6f1f63ccf9d307a7
matthew <matthew@rothl.com>
2021-12-11 16:11:22 +0000

    code with dad

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,22 +1,30 @@
 
-#define first function
+# define first function
 def first_function(msg):
     print(msg)
 
-#define second function
+# define second function
 def sum_two_integers(a, b):
     c = a + b
-    return c 
+    return c
+
+# define math problem
+def math_problem_solver(a, b):
+    c = (-12 * a) / (18 * a * b)
+    return c
+ 
 
 # invoke first function
-msg = "hello world"
-first_function(msg.capitalize)
-
+message = "macintosh"
+first_function(message)
 
 # invoke second function
-
-number_1 = 13 
-number_2 = 21 
+number_1 = 2 
+number_2 = 446 
 sum = sum_two_integers(number_1, number_2)
 
 print(f"{number_1} plus {number_2} is {sum}")
+
+# invoke math problem
+solution = math_problem_solver(-12, 18)
+print(solution)
